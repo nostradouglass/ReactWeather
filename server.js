3a1b66bd4e1cf2d6fb5f4902b4ab575cd31c7b1c
Kelly Douglass <kdouglass@gmail.com>
2017-03-24 14:25:01 -0500

    removed http middleware

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(function(req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'http') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
-        next();
-    }
-});
+// app.use(function(req, res, next) {
+//     if (req.headers['x-forwarded-proto'] === 'http') {
+//         res.redirect('http://' + req.hostname + req.url);
+//     } else {
+//         next();
+//     }
+// });
 
 app.use(express.static('public'));
 
